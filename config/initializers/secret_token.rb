dbe523dcba633d698db003b729ec83770024b0d8
kate <kate.m@verizon.net>
2011-06-21 10:58:40 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+TurtleIntegrated::Application.config.secret_token = 'a00bd88391c3c146225407fd5a37888cb0bc8bfa29e6fd93cac450c5a6e0f09b8a1b2ee45317737d492ea0f9a092569616e1d436c16f2d9271bcf05831b02970'
